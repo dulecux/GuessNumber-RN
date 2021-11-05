adda44b7c622f3d9dc33af93fb0c30629d79b083
dulecux <dusan.culibrk@live.com>
2021-11-05 01:54:37 +0100

    Game logic is finished

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
 import StartGameScreen from './src/screens/StartGameScreen';
+import GameScreen from './src/screens/GameScreen';
+import GameOverScreen from './src/screens/GameOverScreen';
 
 export default function App() {
+
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGameHandler = () => {
+      setGuessRounds(0);
+      setUserNumber(null);
+  };
+
+  const startGameHandler = (selectedNumber) => {
+      setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+      setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if(userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if( guessRounds > 0) {
+    content = <GameOverScreen rounds={guessRounds} userNumber={userNumber} newGame={configureNewGameHandler} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
